7a31eb6e48cc57aa69670e3400afd1d1ede42aef
CharlesC03 <ceciampa@gmail.com>
2020-11-24 12:26:07 -0500

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,26 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: ['@snowpack/plugin-vue', '@snowpack/plugin-dotenv'],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
