3c8d347f61bc5f138537efc849832be4a38e2306
CharlesC03 <ceciampa@gmail.com>
2020-11-25 22:30:51 -0500

    Adding In ffmpeg, input for video, video player, and shotener settings

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import Shortener from './Shortener.vue';
+import Title from './Title.vue';
 
-const app = createApp(App);
-app.mount("#app");
+const main = createApp(Shortener);
+const title = createApp(Title);
+title.mount('#title');
+main.mount('#main');
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept();
   import.meta.hot.dispose(() => {
-    app.unmount();
+    title.unmount();
+    main.unmount();
   });
 }
